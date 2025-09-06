27fabf058797eb7665a07a60aed6f7bedc6ea61f
Hung <duongvihung1995@gmail.com>
2025-09-06 23:56:18 +0900

    ユーザー情報API接続実装完了

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import useFetch from '../hooks/useFetch';
-import { getUserProfile } from '../services/githubService';
+// import { getUserProfile } from '../services/githubService';
 import { downloadCvService } from '../services/downloadCvService';
+import { getUser } from '../services/userService';
+import { NavLink } from 'react-router-dom';
 
 const GitHubIcon = () => (
   <svg width="48" height="48" xmlns="http://www.w3.org/2000/svg" className="mb-2"> {/* Added margin-bottom */}
@@ -17,7 +19,7 @@ const GitHubIcon = () => (
 
 const Hero = () => {
 
-  const {data:user,loading,error} = useFetch(getUserProfile,'DUONGVIHUNG');
+  const {data:user,loading,error} = useFetch(getUser);
   console.log('[Component] Rendering with state:', { user, loading, error });
 
   const cvUrl = downloadCvService({fileName : 'my-cv.pdf'});
@@ -66,43 +68,16 @@ const Hero = () => {
    return (
     <section className="flex flex-col items-center justify-center dark:bg-gray-900">
   <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
-    <a
-      href="#"
-      className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
-      role="alert"
-    >
-      <span className="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">
-        New
-      </span>{" "}
-      <span className="text-sm font-medium">
-        Hello
-      </span>
-      <svg
-        className="ml-2 w-5 h-5"
-        fill="currentColor"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fillRule="evenodd"
-          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-          clipRule="evenodd"
-        />
-      </svg>
-    </a>
+
     <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-      Hello
+      こんにちは、 {user.name}です。
     </h1>
     <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-      Here at Flowbite we focus on markets where technology, innovation, and
-      capital can unlock long-term value and drive economic growth.
+      {user.selfPr}
     </p>
     <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-      <a
-        href="#"
-        className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
-      >
-        私の職務経歴
+      <NavLink to="/project" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+         プロジェクトを見る
         <svg
           className="ml-2 -mr-1 w-5 h-5"
           fill="currentColor"
@@ -115,7 +90,7 @@ const Hero = () => {
             clipRule="evenodd"
           />
         </svg>
-      </a>
+      </NavLink>
       <a
         href={cvUrl}
         download="my-cv.pdf"
@@ -124,18 +99,18 @@ const Hero = () => {
       <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 15v2a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3v-2m-8 1V4m0 12-4-4m4 4 4-4"/>
       </svg>
-      
-        私の履歴ダウンロード
+
+        {" "}経歴をダウンロード
       </a>
     </div>
     <div className="px-4 mx-auto text-center">
           <div className="flex justify-center items-center mt-8 text-gray-500">
             <a
-              href="#"
+              href={user.githubUrl}
               className="flex flex-col items-center hover:text-gray-800 dark:hover:text-gray-400"
             >
               <GitHubIcon />
-              <span>My Github link</span>
+              <span>Github URL</span>
             </a>
           </div>
     </div>
